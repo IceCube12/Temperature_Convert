ef16fbbc374d9abe026d6073e58ac55964e41461
Rovic <civorzonum@gmail.com>
2022-10-14 17:41:55 +0800

    Remove js from html file, move it to js file

--- script.js
+++ script.js
@@ -1,40 +1,42 @@
-// function valu(){
-// 	var x = document.getElementById("celc").value;
-// }
+function CK(x){
+	return (273.15)+(x);
+}
 
-function Sum(){
-	// return val1 + 4;
-	var field1 = document.getElementById("celc").value;
-	var oper = document.getElementById("toConvert").value;
+function KC(x){
+	return parseFloat((x)-(273.15)).toFixed(2);
+}
+
+function CF(x){
+	return ((x*9)/5) + (32);
+}
+
+function FC(x){
+	return parseFloat((x-32) * 5/9).toFixed(4);
+}
+
+function fnl(){
+var field1 = document.getElementById("celc").value;
+var field2 = parseFloat(field1);
+var oper = document.getElementById("toConvert").value;
 
 	if(oper === 'CtoK'){
-		var field2=273.15;
-		var result=parseFloat(field1)+field2;
-		document.getElementById("result").value=result+" \u00B0K";
+		var res = document.getElementById("result").value=CK(field2)+" \u00B0K";
+		result.innerHTML = res;
 	}
 
 	if(oper === 'KtoC'){
-		var field2=273.15;
-		var result=parseFloat(field1)-field2;
-		document.getElementById("result").value=result+" \u00B0C";
+		var res = document.getElementById("result").value=KC(field2)+" \u00B0C";
+		result.innerHTML = res;
 	}
 
 	if(oper === 'CtoF'){
-		var field2 = 9/5;
-		var field3 = 32;
-		var result1 = parseFloat(field1)*field2;
-		var fresult = parseFloat(result1)+field3;
-		document.getElementById("result").value=fresult+" \u00B0F";
-	}
- 
-	if(oper === 'FtoC'){
-		var field2 = 32;
-		var field3 = 5/9;
-		var result1 = parseFloat(field1)-field2;
-		var fresult = parseFloat(result1)*field3;
-		document.getElementById("result").value=fresult+" \u00B0C";
+		var res = document.getElementById("result").value=CF(field2)+" \u00B0F";
+		result.innerHTML = res;
 	}
 
-};
-// document.getElementById("demo").innerHTML = Sum(x);
-// document.getElementById("demo").innerHTML = Sum(4,3);
+	if(oper === 'FtoC'){
+		var res = document.getElementById("result").value=FC(field2)+" \u00B0C";
+		result.innerHTML = res;
+	}
+
+}
